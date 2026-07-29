d8f2f4a2178a020444bc841cffc53ae6140e4194
Apurv Vyas <apurv.vyas@X1JYHW7XG9.local>
2026-07-30 02:24:48 +0530

    fixed terraform fmt formatting

--- variable.tf
+++ variable.tf
@@ -1,23 +1,23 @@
 variable "vpc_cidr" {
-  default  = "10.0.0.0/16"
-  }
+  default = "10.0.0.0/16"
+}
 
-  variable "aws_region" {
-    default = "ap-south-1"
-  }
+variable "aws_region" {
+  default = "ap-south-1"
+}
 
-  variable "azs" {
-    default = ["ap-south-1a", "ap-south-1b"]
-  }
+variable "azs" {
+  default = ["ap-south-1a", "ap-south-1b"]
+}
 
-  variable "public_subnet_cidrs" {
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
-  }
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
 
-  variable "private_subnet_cidrs" {
-    default = ["10.0.101.0/24", "10.0.102.0/24"]
-  }
+variable "private_subnet_cidrs" {
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
 
-  variable "instance_type" {
-    default = "t3.micro"
-  }
+variable "instance_type" {
+  default = "t3.micro"
+}
